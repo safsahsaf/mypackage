d52377d053a98cabf99c6892d40c3d4183c63fac
safae <sahbani.safae@gmail.com>
2024-02-21 16:32:52 +0100

    change in typo

--- mypackage/myModule.py
+++ mypackage/myModule.py
@@ -1,23 +1,22 @@
-def top_n (items,n):
-    """Return the top n items in an array , in desc order.
+def top_n(items, n):
+    """Return the top n items in an array, in descending order.
 
-    Args: 
-    items (array): list or array like object
-    n(int): num of items to Return
+    Args:
+    items (array): list or array-like object
+    n (int): number of items to return
 
-    Return:
-     array : top n items, in desc order
+    Returns:
+    array: top n items, in descending order
 
-     Egs:
-      >>> top_n([8,3,2,7,4],3)
-      [8,7,3]
-      """
-      for in in range(n):
-          for j in range(len(items)-1-i):
+    Examples:
+    >>> top_n([8, 3, 2, 7, 4], 3)
+    [8, 7, 4]
+    """
+    for i in range(n):
+        for j in range(len(items) - 1 - i):
+            if items[j] > items[j + 1]:
+                items[j], items[j + 1] = items[j + 1], items[j]
 
-              if items[j]> items[j+1]:
-                  items [j], items[j+1]= items[j+1], items[j]
+    top_n = items[-n:]
 
-                  top_n= items[-n:]
-
-                  Return top_n[[::-1]
+    return top_n[::-1]
